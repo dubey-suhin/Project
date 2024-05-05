39457abc0bcc1de488772bdcf1eb880a85fdeada
dubey-suhin <115923263+dubey-suhin@users.noreply.github.com>
2024-05-05 13:39:46 +0530

    Delete Stegano_cv.py

--- Stegano_cv.py
+++
@@ -1,9 +0,0 @@
-from stegano import lsb
-from stegano.lsb import generators
-
-# message = 'Hi there , Suhin is here'
-# hide = lsb.hide('flower.jpg', message, generators.eratosthenes())
-# hide.save('hidden_image.png')
-
-unhide = lsb.reveal('hidden_image.png', generators.eratosthenes())
-print(unhide)
